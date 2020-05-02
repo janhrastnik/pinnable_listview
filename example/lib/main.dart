549a98c1b3915f9778005e544f294a63107b6c5f
Jan Hrastnik <jan.hrastnik2@gmail.com>
2020-05-02 16:14:16 +0200

    moved pinnable listview

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pinnablelistview/pinlistview.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  PinController pinController = PinController();
+
+  Widget tileWidget(i) {
+    return GestureDetector(
+      child: Container(
+        height: 50.0,
+        color: Colors.blue[100 + i*100],
+        child: Center(child: Text(i.toString()),)
+      ),
+      onTap: () {
+        pinController.pin(i);
+      },
+    );
+  }
+
+  List<Widget> testList = [
+    GestureDetector(
+      child: Container(
+        color: Colors.red,
+      ),
+      onTap: () {
+      },
+    ),
+    GestureDetector(
+      child: Container(
+        color: Colors.red,
+      ),
+      onTap: () {
+      },
+    ),
+    GestureDetector(
+      child: Container(
+        color: Colors.red,
+      ),
+      onTap: () {
+      },
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,210 +53,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PinList(children: <Widget>[
-        Container(
-          height: 50.0,
-          color: Colors.red,
-          child: Text("A"),
+      home: Scaffold(
+        body: PinnableListView(
+          pinController: pinController,
+          children: Iterable<int>.generate(5).map((i) => tileWidget(i)).toList()
         ),
-        Container(
-          height: 50.0,
-          color: Colors.amber,
-          child: Text("B"),
-
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            pinController.pin(4);
+          },
         ),
-        Container(
-          height: 50.0,
-          color: Colors.green,
-          child: Text("C"),
-
-        ),
-        Container(
-          height: 50.0,
-          color: Colors.blue,
-          child: Text("D"),
-
-        ),
-        Container(
-          height: 50.0,
-          color: Colors.purple,
-          child: Text("E"),
-
-        )
-      ],),
+      )
     );
   }
 }
-
-class PinList extends StatefulWidget {
-  final List<Widget> children;
-  List<double> widgetHeightList = List();
-  List<Function> animFunctions = List();
-  int pinned;
-  PinList({Key key, this.children, this.pinned}) : super(key: key);
-  List<int> originalIndexes;
-
-  @override
-  PinListState createState() => PinListState();
-}
-
-class PinListState extends State<PinList> {
-
-  addFunction(Function function) {
-    print("event");
-    widget.animFunctions.add(function);
-  }
-
-  callFunctions(int index) {
-      print(widget.animFunctions.length);
-      if (widget.pinned == null || index != 0) {
-        widget.animFunctions.sublist(0, index).forEach((function) {
-            function(50.0, false);
-        });
-        Future.delayed(Duration(seconds: 1)).then((_) {
-          setState(() {
-            widget.pinned = widget.originalIndexes[index];
-            int moveIndex = widget.originalIndexes[index];
-            widget.originalIndexes.removeAt(index);
-            widget.originalIndexes.insert(0, moveIndex);
-
-            Widget moveWidget = widget.children[index];
-            print(moveWidget);
-            widget.children.removeAt(index);
-            widget.children.insert(0, moveWidget);
-            print(widget.originalIndexes);
-          });
-        });
-      } else {
-        print("this gets run");
-        widget.animFunctions.sublist(1, widget.originalIndexes[index]+1).forEach((function) {
-          function(50.0, true);
-        });
-        Future.delayed(Duration(seconds: 1)).then((_) {
-          setState(() {
-            widget.pinned = null;
-            int moveIndex = widget.originalIndexes[0];
-            widget.originalIndexes.removeAt(0);
-            widget.originalIndexes.insert(moveIndex, moveIndex);
-
-            Widget moveWidget = widget.children[0];
-            print(moveWidget);
-            widget.children.removeAt(0);
-            widget.children.insert(moveIndex, moveWidget);
-            print(widget.originalIndexes);
-          });
-        });
-      }
-  }
-
-  getData(int index) {
-    double distance;
-    if (widget.pinned == widget.originalIndexes[index]) {
-      distance = -widget.pinned * 50.0;
-    } else {
-      distance = index * 50.0;
-    }
-    return [distance, widget.originalIndexes[index]];
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    for (Widget w in widget.children) {
-      w.
-    }
-    widget.originalIndexes = Iterable<int>.generate(widget.children.length).toList();
-    if (widget.pinned != null) {
-      int moveIndex = widget.originalIndexes[widget.pinned];
-      widget.originalIndexes.removeAt(widget.pinned);
-      widget.originalIndexes.insert(0, moveIndex);
-
-      Widget moveWidget = widget.children[widget.pinned];
-      print(moveWidget);
-      widget.children.removeAt(widget.pinned);
-      widget.children.insert(0, moveWidget);
-      print(widget.originalIndexes);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: widget.children.length,
-        itemBuilder: (BuildContext context, int index) {
-          return PinWidget(
-              child: widget.children[index],
-              index: index,
-              addFunction: addFunction,
-              callFunctions: callFunctions,
-              getData: getData,
-          );
-        },
-      ),
-    );
-  }
-}
-
-class PinWidget extends StatefulWidget {
-  final Widget child;
-  final int index;
-  final Function(Function function) addFunction;
-  final Function(int index) callFunctions;
-  final Function(int index) getData;
-  PinWidget({Key key, this.child, this.index, this.addFunction,
-    this.getData, this.callFunctions}): super(key: key);
-  PinWidgetState createState() => PinWidgetState();
-}
-
-class PinWidgetState extends State<PinWidget> with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  double distance = 0.0;
-
-  move(double height, bool isPinned) {
-    if (isPinned) {
-      distance = height;
-    } else {
-      distance = -height;
-    }
-    animationController.forward();
-    Future.delayed(Duration(seconds: 1)).then((_) {
-      animationController.reset();
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1)
-    );
-    widget.addFunction(move);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: animationController,
-        builder: (BuildContext context, Widget child) {
-          return Transform.translate(
-              offset: Offset(1.0, -distance * animationController.value),
-              child: GestureDetector(
-                child: widget.child,
-                onTap: () {
-                  distance = widget.getData(widget.index)[0];
-                  print(distance);
-                  int originalIndex = widget.getData(widget.index)[1];
-                  widget.callFunctions(widget.index);
-                  animationController.forward();
-                  Future.delayed(Duration(seconds: 1)).then((_) {
-                    animationController.reset();
-                  });
-                },
-              )
-          );
-        });
-  }
-}
-
--- lib/pinlistview.dart
+++ lib/pinlistview.dart
@@ -1,9 +1,207 @@
-library pinlistview;
+library pinnablelistview;
 import 'package:flutter/material.dart';
 
-class Ye extends StatelessWidget {
+class PinnableListView extends StatefulWidget {
+  final List<Widget> children;
+  List<GlobalKey> globalKeys = List();
+  List<Function> animFunctions = List();
+  List<Function> pinFunctions = List();
+  List<int> originalIndexes = List();
+  PinController pinController;
+  int pinned;
+  PinnableListView({Key key, @required this.children, this.pinned, @required this.pinController}) : super(key: key);
+
+  @override
+  PinnableListViewState createState() => PinnableListViewState();
+}
+
+class PinnableListViewState extends State<PinnableListView> {
+
+  addData(Function animFunction, Function pinFunction, GlobalKey key) {
+    widget.globalKeys.add(key);
+    widget.animFunctions.add(animFunction);
+    widget.pinFunctions.add(pinFunction);
+  }
+
+  callFunctions(int index) {
+    if (widget.pinned == null || index != 0) {
+      widget.animFunctions.sublist(0, index).forEach((function) {
+        double widgetHeight = widget.globalKeys[index].currentContext.findRenderObject().paintBounds.height;
+        function(widgetHeight, false);
+      });
+      Future.delayed(Duration(milliseconds: 600)).then((_) {
+        setState(() {
+          widget.pinned = widget.originalIndexes[index];
+          int moveIndex = widget.originalIndexes[index];
+          widget.originalIndexes.removeAt(index);
+          widget.originalIndexes.insert(0, moveIndex);
+
+          Widget moveWidget = widget.children[index];
+          widget.children.removeAt(index);
+          widget.children.insert(0, moveWidget);
+        });
+      });
+    } else {
+      widget.animFunctions.sublist(1, widget.originalIndexes[index]+1).forEach((function) {
+        double widgetHeight = widget.globalKeys[index].currentContext.findRenderObject().paintBounds.height;
+        function(widgetHeight, true);
+      });
+      Future.delayed(Duration(milliseconds: 600)).then((_) {
+        setState(() {
+          widget.pinned = null;
+          int moveIndex = widget.originalIndexes[0];
+          widget.originalIndexes.removeAt(0);
+          widget.originalIndexes.insert(moveIndex, moveIndex);
+
+          Widget moveWidget = widget.children[0];
+          widget.children.removeAt(0);
+          widget.children.insert(moveIndex, moveWidget);
+        });
+      });
+    }
+  }
+
+  getData(int index) {
+    double distance;
+    double widgetHeight = widget.globalKeys[index].currentContext.findRenderObject().paintBounds.height;
+    if (widget.pinned == widget.originalIndexes[index]) {
+      distance = -widget.pinned * widgetHeight;
+    } else {
+      distance = index * widgetHeight;
+    }
+    return [distance, widget.originalIndexes[index]];
+  }
+
+  setPin() {
+    int moveIndex = widget.originalIndexes[widget.pinned];
+    widget.originalIndexes.removeAt(widget.pinned);
+    widget.originalIndexes.insert(0, moveIndex);
+
+    Widget moveWidget = widget.children[widget.pinned];
+    widget.children.removeAt(widget.pinned);
+    widget.children.insert(0, moveWidget);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.originalIndexes = Iterable<int>.generate(widget.children.length).toList();
+    widget.pinController.addListener(() {
+      int trueIndex = widget.originalIndexes.indexOf(widget.pinController.index);
+      widget.pinFunctions[trueIndex]();
+    });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (widget.pinned != null) {
+        setPin();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ReorderableListView(children: null, onReorder: null)
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: widget.children.length,
+        itemBuilder: (BuildContext context, int index) {
+          return PinWidget(
+            child: widget.children[index],
+            index: index,
+            addData: addData,
+            callFunctions: callFunctions,
+            getData: getData,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PinWidget extends StatefulWidget {
+  // widget used for pinnable listview to handle individual events on children
+  final Widget child;
+  final int index;
+  final Function(Function animFunction, Function pinFunction, GlobalKey key) addData;
+  final Function(int index) callFunctions;
+  final Function(int index) getData;
+  PinWidget({Key key, this.child, this.index, this.addData,
+    this.getData, this.callFunctions}): super(key: key);
+  PinWidgetState createState() => PinWidgetState();
+}
+
+class PinWidgetState extends State<PinWidget> with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  var tween;
+  GlobalKey key;
+  double distance = 0.0;
+
+  move(double height, bool isPinned) {
+    // called when a different pin widget gets pinned/unpinned, so that this one moves up or down
+    if (isPinned) {
+      distance = height;
+    } else {
+      distance = -height;
+    }
+    animationController.forward();
+    Future.delayed(Duration(milliseconds: 600)).then((_) {
+      animationController.reset();
+    });
+  }
+
+  pin() {
+    // called when this widget should get pinned/unpinned
+    distance = widget.getData(widget.index)[0];
+    widget.callFunctions(widget.index);
+    animationController.forward();
+    Future.delayed(Duration(milliseconds: 600)).then((_) {
+      animationController.reset();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    key = GlobalKey();
+    animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 600)
+    );
+    tween = Tween(
+        begin: 0.0,
+        end: 1.0
+    ).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Curves.ease
+    ));
+    widget.addData(move, pin, key);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (BuildContext context, Widget child) {
+          return Transform.translate(
+              offset: Offset(1.0, -distance * tween.value),
+              child: GestureDetector(
+                child: Container(
+                  child: widget.child,
+                  key: key,
+                ),
+                onTap: () {
+                  pin();
+                },
+              )
+          );
+        });
+  }
+}
+
+class PinController extends ChangeNotifier {
+  // used to handle pin events on pinnable listview
+  int index;
+
+  pin(int i) {
+    index = i;
+    notifyListeners();
   }
 }
